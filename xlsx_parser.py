196badeeb6a74dee3123d0e9e27f429245baae70
socloseeee <65871712+socloseeee@users.noreply.github.com>
2023-03-18 01:41:00 +0300

    rework input + adapted to network needs

--- xlsx_parser.py
+++ xlsx_parser.py
@@ -5,16 +5,25 @@ import numpy as np
 pd.set_option('display.max_rows', None)
 #pd.set_option('display.max_columns', None)
 
-columns = ('Номер заявки', 'Статус', 'Услуга', 'Дата регистрации заявки')  # Клиент*
-dates = ('09.03.23', '10.03.23', '12.03.23', '13.03.23', '14.03.23', '15.03.23')
-files_data = {
-    data: pd.read_excel(
-        f'../hackathon_-application_life/xlsx_files/Аудит заявок РФ_{dates[i]}.xlsx', usecols=columns
-    ) for data, i in zip(dates, range(sum(1 for x in Path('xlsx_files').iterdir())))
-}
 
-print(
-    files_data['09.03.23']['Номер заявки'].value_counts()[:5],
-    '',
-    *(files_data[data].loc[files_data[data]['Номер заявки'] == 50905108] for data in dates), sep='\n'
-)
+def read_data(columns, dates):
+    data, numbers = {}, set()
+    for date, i in zip(dates, range(sum(1 for x in Path('xlsx_files').iterdir()))):
+        data[date] = pd.read_excel(Path(f"xlsx_files/Аудит заявок РФ_{dates[i]}.xlsx"), usecols=columns)
+        numbers = numbers | set(data[date]['Номер заявки'].values)
+    return data, numbers
+
+
+if __name__ == "__main__":
+    columns: tuple = ('Номер заявки', 'Статус', 'Услуга', 'Дата регистрации заявки')  # Клиент*
+    dates: tuple = ('09.03.23', '10.03.23', '12.03.23', '13.03.23', '14.03.23', '15.03.23')
+    files_data, numbers = read_data(columns, dates)
+    print(files_data[dates[0]]['Номер заявки'].value_counts()[:5], '\n')  # топ 5 значений по повторам в первом файле
+    while True:
+        applyment = np.int64(input('Введите номер заявки > '))
+        if applyment in numbers:
+            break
+        print('Некорректный ввод!')
+    print(
+        *(files_data[data].loc[files_data[data]['Номер заявки'] == applyment] for data in dates), sep='\n'
+    )
