3aabfb14e6bff7f0f45c95f77e77c7b6195d683c
socloseeee <65871712+socloseeee@users.noreply.github.com>
2023-03-18 22:48:22 +0300

    start month, end month start year, end year Update!

--- xlsx_parser.py
+++ xlsx_parser.py
@@ -42,42 +42,27 @@ def form_date(dates) -> tuple:
 
     start_day: int
     end_day: int
-    month: str
-    year: str
+    start_month: int
+    end_month: int
+    start_year: int
+    end_year: int
 
     days, monthes, years = [], set(), set()
     for day, month, year in [elem.split('.') for elem in dates]:
         days.append(int(day))
-        monthes.add(month)
-        years.add(year)
-
-    # Выбор месяца
-    while True:
-        month = str(sys.argv[3])  # return_value('Введите месяц > ', int)
-        if month in monthes:
-            break
-        print('Неккоректный ввод!')
-        sys.exit(1)
-
-    # Выбор года
-    while True:
-        year = str(sys.argv[4])  # return_value('Введите год > ', int)
-        if month in monthes:
-            break
-        print('Неккоректный ввод!')
-        sys.exit(1)
-
+        monthes.add(int(month))
+        years.add(int(year))
     # Проверка корректности ввода и ввод дней
     try:
         while True:
             while True:
-                start_day = int(sys.argv[1])  #return_value('Введите начальный день > ', int)
+                start_day = int(sys.argv[1])  # return_value('Введите начальный день > ', int)
                 if 0 < start_day < 31:
                     break
                 print('Неккоректный день!')
                 sys.exit(1)
             while True:
-                end_day = int(sys.argv[2])  #return_value('Введите конечный день > ', int)
+                end_day = int(sys.argv[2])  # return_value('Введите конечный день > ', int)
                 if 0 < end_day < 31 and end_day >= start_day:
                     break
                 print('Неккоректный день!')
@@ -86,15 +71,70 @@ def form_date(dates) -> tuple:
                 if elem in range(start_day, end_day + 1):
                     raise Exception
             else:
-                sys.exit(1)
                 print('В данные дни отсутствуют файлы аудита!')
+                sys.exit(1)
     except Exception:
-        sys.exit(1)
+        pass
+
+    # Выбор месяца
+    try:
+        while True:
+            while True:
+                start_month = int(sys.argv[3])  #return_value('Введите начальный день > ', int)
+                if 0 < start_month < 12:
+                    break
+                print('Неккоректный месяц!')
+                sys.exit(1)
+            while True:
+                end_month = int(sys.argv[4])  #return_value('Введите конечный день > ', int)
+                if 0 < end_month < 12 and end_month >= start_month:
+                    break
+                print('Неккоректный месяц!')
+                sys.exit(1)
+            for elem in monthes:
+                if elem in range(start_month, end_month + 1):
+                    print(elem, start_month, end_month + 1)
+                    raise Exception
+            else:
+                print('В данные месяцы отсутствуют файлы аудита!')
+                sys.exit(1)
+    except Exception:
+        pass
+
+    # Выбор года
+    try:
+        while True:
+            while True:
+                start_year = int(sys.argv[5])
+                if 0 < start_year < 24:
+                    break
+                print('Неккоректный год!')
+                sys.exit(1)
+            while True:
+                end_year = int(sys.argv[6])  # return_value('Введите конечный день > ', int)
+                if 0 < end_year < 24 and end_year >= start_year:
+                    break
+                print('Неккоректный год!')
+                sys.exit(1)
+            for elem in years:
+                if elem in range(start_year, end_year + 1):
+                    raise Exception
+            else:
+                print('В данные годы отсутствуют файлы аудита!')
+                sys.exit(1)
+    except Exception:
+        pass
 
     start_day = '0' + str(start_day) if start_day < 10 else str(start_day)
     end_day = '0' + str(end_day) if end_day < 10 else str(end_day)
 
-    return (f"{start_day}.{month}.{year}", f"{end_day}.{month}.{year}")
+    start_month = '0' + str(start_month) if start_month < 10 else str(start_month)
+    end_month = '0' + str(end_month) if end_month < 10 else str(end_month)
+
+    start_year = '0' + str(start_year) if start_year < 10 else str(start_year)
+    end_year = '0' + str(end_year) if end_year < 10 else str(end_year)
+
+    return (f"{start_day}.{start_month}.{start_year}", f"{end_day}.{end_month}.{end_year}")
 
 
 def selection_of_period(date_slice, dates) -> tuple:
@@ -118,13 +158,13 @@ def selection_of_period(date_slice, dates) -> tuple:
 
 
 def dates_read_from_files() -> tuple:
-    content = os.listdir(Path("data"))
+    content = os.listdir(Path("data"))  # сделать переменной
     return tuple(data[data.index('_') + 1:data.index('.xlsx')] for data in content)
 
 
 if __name__ == "__main__":
-    # print('dsadas')
-    if len(sys.argv) == 7:
+    print(sys.argv)
+    if len(sys.argv) == 9:
         columns: tuple = (
             'Номер заявки', 'Клиент*' ,'ИНН', 'Статус', #'Дата входа заявки в статус', 'Услуга', 'Дата регистрации заявки',
             #'Дата регистрации под заявки', 'Рег. наряда на ТВП', 'Дата отклонения под заявки', 'Тип проверки ТВП',
@@ -148,7 +188,7 @@ if __name__ == "__main__":
         print(files_data[dates[0]]['Номер заявки'].value_counts()[:5], '\n')  # топ 5 значений по повторам в первом файле
         print(files_data[dates[0]]['ИНН'].value_counts(), '\n')
 
-        INN = np.int64(sys.argv[5])  # return_value('ИНН > ', np.int64, INNs)
+        INN = np.int64(sys.argv[7])  # return_value('ИНН > ', np.int64, INNs)
 
         INN_dict: dict = {}
 
@@ -158,7 +198,7 @@ if __name__ == "__main__":
         print('ИНН cловарь:')
         print(*(INN_dict[data] for data in selected_period), sep='\n\n')
 
-        applyment_number = np.int64(sys.argv[6])  #return_value('Введите номер заявки > ', np.int64, numbers)
+        applyment_number = np.int64(sys.argv[8])  #return_value('Введите номер заявки > ', np.int64, numbers)
         applyment_dict: dict = {}
         values = []
         # Сортируем по номеру заявки
@@ -182,4 +222,5 @@ if __name__ == "__main__":
         print('\nЖизненный цикл:\n', application_life)
         sys.exit(0)
     else:
+        print('Не совпадает количество переменных из консольного ввода!')
         sys.exit(1)
