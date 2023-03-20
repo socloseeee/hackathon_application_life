6ea5ec0031cae025528c4fe3127700f3bb809a5b
socloseeee <65871712+socloseeee@users.noreply.github.com>
2023-03-20 12:43:29 +0300

    view issues

--- xlsx_parser.py
+++ xlsx_parser.py
@@ -10,38 +10,23 @@ import warnings
 warnings.simplefilter("ignore")
 
 
-pd.set_option('display.max_colwidth', 20)
-pd.set_option('display.max_rows', 10)
+pd.set_option('display.max_colwidth', None)
+pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', 10)
 pd.options.display.float_format ='{:.0f}'.format
 
 
 def read_data(columns, dates) -> tuple:
+
     data, numbers, INN = {}, set(), set()
+
     for date, i in zip(dates, range(sum(1 for x in Path('data').iterdir()))):
         data[date] = pd.read_excel(
             Path(f"data/_{dates[i]}.xlsx"),
-            # dtype={'Наряд КУРС':None, 'Номер ОТА ШПД':None},
             usecols=columns
         )
-        numbers = numbers | set(data[date]['Номер заявки'].values)
-        INN = INN | set(data[date]['ИНН'].values)
-    return data, numbers, INN
 
-
-def return_value(msg, type, check=None, val=None) -> [int, str]:
-    while True:
-        try:
-            value = type(input(msg))
-        except Exception:
-            print('Неккоректный ввод!')
-            continue
-        if check == None:
-            break
-        if value in check:
-            break
-        print('Некорректный ввод!')
-    return value
+    return data
 
 
 def form_date(dates, start_date=None, end_date=None) -> tuple:
@@ -64,6 +49,7 @@ def form_date(dates, start_date=None, end_date=None) -> tuple:
         days.append(int(day))
         monthes.add(int(month))
         years.add(int(year))
+
     # Проверка корректности ввода и ввод дней
     try:
         while True:
@@ -169,8 +155,89 @@ def selection_of_period(date_slice, dates) -> tuple:
 
 
 def dates_read_from_files(path=None) -> tuple:
+
     content = os.listdir(path if path is not None else Path("data"))  # сделать переменной
+
     return tuple(data[data.index('_') + 1:data.index('.xlsx')] for data in content)
+
+
+def INN_sort(INN, INN_dict, values) -> dict:
+
+    INN = np.int64(INN)
+    INN_response: list = []
+
+    # Сортируем по ИНН
+    for date in selected_period:
+        value = files_data[date].loc[files_data[date]['ИНН'] == INN]
+        if not value.empty:
+            values.append((value, date))
+            INN_response.append({'date': date})
+            for col in columns:
+                INN_response[-1][col] = value[col].values
+            INN_dict[date] = files_data[date].loc[files_data[date]['ИНН'] == INN]
+    if len(values) == 0:
+        print('Неккоректный ИНН!')
+        sys.exit(1)
+
+    return INN_dict, values
+
+
+def applyment_sort(applyment_number, applyment_dict, values):
+
+    applyment_number = np.int64(applyment_number)
+    applyment_response: list = []
+    values = []
+
+    # Сортируем по номеру заявки
+    for date in selected_period:
+        value = INN_dict[date].loc[INN_dict[date]['Номер заявки'] == applyment_number]
+        if not value.empty:
+            values.append((value, date))
+            applyment_response.append({'date': date})
+            for col in columns:
+                applyment_response[-1][col] = value[col].values
+            applyment_dict[date] = value
+    if len(values) == 0:
+        print('Неккоректный номер заявки!')
+        sys.exit(1)
+
+    return applyment_dict, values
+
+
+def create_application_life_data(values):
+
+    application_life = pd.DataFrame(columns=columns)
+
+    for i in range(len(values)):
+        if not values[i][0].empty:
+            application_life = pd.concat([application_life, pd.DataFrame(values[i][0], columns=columns)])
+            application_life['date'] = values[i][1]
+    application_life = application_life.drop_duplicates(subset=['Статус'])
+
+    return application_life
+
+
+def create_application_response(application_life):
+
+    application_response: list = []
+
+    for i in range(application_life.shape[0]):
+        application_response.append({'date': application_life['date'].iloc[i]})
+        for elem in application_life:
+            application_response[-1][elem] = application_life[elem].iloc[i]
+
+    return application_response
+
+
+def application_changes(selected_period, files_data, values):
+
+    new_data = pd.DataFrame(columns=columns)
+
+    for date in selected_period:
+        new_data = pd.concat([new_data, files_data[date]])
+
+    return new_data[['Номер заявки', 'Клиент*', 'Статус']].drop_duplicates(
+        subset=['Номер заявки', 'Клиент*', 'Статус']).value_counts()[:5]
 
 
 def createParser(arg_names) -> argparse:
@@ -196,6 +263,7 @@ if __name__ == "__main__":
     third_issue = {'start_date', 'end_date', 'INN', 'order'}
 
     if set(arg_names) in (first_issue, second_issue, third_issue):
+
         columns: tuple = (
               'Номер заявки', 'Клиент*' ,'ИНН', 'Статус',# 'Дата входа заявки в статус', 'Услуга', 'Дата регистрации заявки',
             # 'Дата регистрации под заявки', 'Рег. наряда на ТВП', 'Дата отклонения под заявки', 'Тип проверки ТВП',
@@ -203,95 +271,53 @@ if __name__ == "__main__":
             # 'Дата окончания АПТВ планируемая', 'Дата окончания АПТВ фактическая', 'Длительность этапа АПТВ',
             # 'Дата отправки на ДО', 'Дата окончания ДО планируемая', 'Дата окончания ДО фактическая', 'Длительность этапа ДО'
         )
+
+        # Все даты считанные с файлов
         dates: tuple = dates_read_from_files(args.path if 'path' in arg_names else None)  # написать функцию считывающие с файлов дату
-        # print(dates)
 
+        # Формируем срез по датам
         date_slice: tuple = form_date(dates, start_date=values[0], end_date=values[1])
-        # print(date_slice)
+
+        # Массив дат соответствующий датам в названиях файлов)
         selected_period: tuple = selection_of_period(date_slice, dates)
-        # print(selected_period, '\n')
 
-        files_data, numbers, INNs = read_data(columns, selected_period)
+        # Парсинг даты по xlsx файлам
+        files_data: dict = read_data(columns, selected_period)
 
+        # Избавление от html тегов в колонке 'Клиент*'
         for date in selected_period:
             files_data[date]['Клиент*'] = files_data[date]['Клиент*'].str.replace('[<span style="color: red;padding:2px">, </span>]', '', regex=True)
 
-        # print(files_data[dates[-3]]['Клиент*'])
-        # files_data[dates[0]]['ИНН'] = files_data[dates[0]]['ИНН'].astype(np.int32)
-        # print(files_data)
-
-        # while True:
-        # print(files_data[dates[0]]['Номер заявки'].value_counts()[:5], '\n')  # топ 5 значений по повторам в первом файле
-        # print(files_data[dates[0]]['ИНН'].value_counts(), '\n')
-
+        # Задаём ИНН
         INN = None if 'INN' not in arg_names else args.INN  # return_value('ИНН > ', np.int64, INNs)
         INN_dict: dict = {}
-        values = []
+        values: list = []
 
+        # Cортируем если ИНН задан, иначе копируем данные из мейн спаршенной переменной
         if INN != None:
-            INN = np.int64(INN)
-            INN_response: list = []
-
-            # Сортируем по ИНН
-            for date in selected_period:
-                value = files_data[date].loc[files_data[date]['ИНН'] == INN]
-                if not value.empty:
-                    values.append((value, date))
-                    INN_response.append({'date': date})
-                    for col in columns:
-                        INN_response[-1][col] = value[col].values
-                    INN_dict[date] = files_data[date].loc[files_data[date]['ИНН'] == INN]
-            if len(values) == 0:
-                print('Неккоректный ИНН!')
-                sys.exit(1)
-            # print('ИНН (response):')
-            # print(INN_response, sep='\n\n')
+            INN_dict, values = INN_sort(INN, INN_dict, values)
         else:
-            INN_dict = files_data.copy()
-        # print(args.order)
+            INN_dict, values = files_data.copy(), []
 
+
+        # Задаём номер заявки
         applyment_number = None if 'order' not in arg_names else args.order  #return_value('Введите номер заявки > ', np.int64, numbers)
         applyment_dict: dict = {}
 
+        # Сортиуем если номер заявки задан, иначе копируем данные из мейн спаршенной переменной
         if applyment_number != None:
-            applyment_number = np.int64(applyment_number)
-            applyment_response: list = []
-            values = []
-            # Сортируем по номеру заявки
-            # print('\nНомер заявки (response):')
-            for date in selected_period:
-                value = INN_dict[date].loc[INN_dict[date]['Номер заявки'] == applyment_number]
-                if not value.empty:
-                    values.append((value, date))
-                    applyment_response.append({'date': date})
-                    for col in columns:
-                        applyment_response[-1][col] = value[col].values
-                    applyment_dict[date] = value
-            # print(values)
-            if len(values) == 0:
-                print('Неккоректный номер заявки!')
-                sys.exit(1)
-            # print(applyment_response, sep='\n\n')
+            applyment_dict, values = applyment_sort(applyment_number, applyment_dict, values)
         else:
             applyment_dict = INN_dict.copy()
 
-        application_life = pd.DataFrame(values[0][0], columns=columns)
-
-        for i in range(1, len(values)):
-            if not values[i][0].empty:
-                application_life = pd.concat([application_life, pd.DataFrame(values[i][0], columns=columns)])
-                application_life['date'] = values[i][1]
-
-
-        application_life = application_life.drop_duplicates(subset=['Статус'])
-        application_response: list = []
-
-        for i in range(application_life.shape[0]):
-            application_response.append({'date': application_life['date'].iloc[i]})
-            for elem in application_life:
-                application_response[-1][elem] = application_life[elem].iloc[i]
-        # print('\nЖизненный цикл (response):\n')
+        # Формирование жизни заявки
+        application_life = create_application_life_data(values)
+        application_response: list = create_application_response(application_life)
         print(application_response)
+
+        # Поиск заявок имеющих наибольшее количество изменения статуса среди всех файлов
+        print(application_changes(selected_period, files_data, values))
+
         sys.exit(0)
     else:
         print('Не совпадает количество переменных из консольного ввода!')
